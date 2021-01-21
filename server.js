0a0d25f7e882612c4dfc23e8e415d5850d6d84c9
jablonskik <k.jablonski@telejur.net>
2021-01-21 17:36:52 +0100

    werfref

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const port = process.env.port || '8080'
 const app = express();
 
+app.set("port", port);
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/weather-globe-test'));
@@ -13,4 +14,3 @@ app.get('*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/weather-globe-test/index.html'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
